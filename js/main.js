b7e2a35a1ca02b3f152f321f5bad5572f4492ea8
davidabutler92 <davidabutler92@yahoo.com>
2020-03-28 14:18:16 -0700

    add pull var values and console.log to check if working

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
   
+  $("form#survey").submit(function(event) {
+    event.preventDefault();
+    
+    var userName = $("#name").val();
+    var userAge = $("#age").val();
+    var netflixRate = parseInt($("#netflix").val());
+    var fruitRate = parseInt($("#fruit").val());
+    var travelRate = parseInt($("#travel").val());
+    var total = netflixRate + fruitRate + travelRate; 
+
+
+  });
 });
